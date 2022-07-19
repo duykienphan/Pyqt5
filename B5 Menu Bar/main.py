276844989dea2bfff6228d553f0a145d4b0f36b5
kienphan301 <kienphan30103@gmail.com>
2022-07-19 21:07:37 +0700

    ok

---
+++ B5 Menu Bar/main.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from gui import Ui_MainWindow
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+
+        #Cấu hình chức năng cho các nút bấm
+        self.uic.actionTab_1.triggered.connect(self.func_showTab1)
+        self.uic.actionTab_2.triggered.connect(self.func_showTab2)
+        self.uic.actionTab_3.triggered.connect(self.func_showTab3)
+        self.uic.btn_close.clicked.connect(self.func_closeWindow)
+
+        #Cấu hình lại đường dẫn cho ảnh, icon
+        self.uic.lbl_img1.setPixmap(QtGui.QPixmap(r"D:\Kien Phan\Python\Pyqt5\B3 Stacked Widget\eiffel.jpg"))
+        self.uic.lbl_img2.setPixmap(QtGui.QPixmap(r"D:\Kien Phan\Python\Pyqt5\B3 Stacked Widget\riviera.jpg"))
+
+    ##################### Các chức năng #############################
+    def func_showTab1(self):
+        self.uic.tabWidget.setCurrentWidget(self.uic.tab)
+    def func_showTab2(self):
+        self.uic.tabWidget.setCurrentWidget(self.uic.tab_2)
+    def func_showTab3(self):
+        self.uic.tabWidget.setCurrentWidget(self.uic.tab_3)
+    def func_closeWindow(self):
+        self.main_win.close()
+    #################################################################    
+
+    def show(self):
+        self.main_win.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
